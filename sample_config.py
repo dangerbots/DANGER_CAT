45c70049888f7e5fb647ae81888bd9a8bc1224e3
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-09-05 20:31:27 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -30,7 +30,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Kolkata")
     # set this with required cat repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Jisan09/catuserbot.git"
+        "UPSTREAM_REPO", "https://github.com/dangerbots/DANGER_CAT"
     )
 
     # BASIC and MAIN CONFIG VARS
