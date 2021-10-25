45bc7d31097ae779ecd143381e2766192aabfa29
Anonymous么kid <79362039+dangerbots@users.noreply.github.com>
2021-10-25 23:07:53 +0530

    Update ping.py

--- userbot/plugins/ping.py
+++ userbot/plugins/ping.py
@@ -35,7 +35,7 @@ async def _(event):
         end = datetime.now()
         ms = (end - start).microseconds / 1000
         await catevent.edit(
-            f"<b><i>☞ Pong</b></i>\n➥ {ms} <b><i>ms\n➥ Bot of {hmention}</b></i>",
+            f"<b><i>☞ 🅿🅾🅽🅶</b></i>\n➥ {ms} <b><i>ms\n➥ Bot of {hmention}❣️</b></i>",
             parse_mode="html",
         )
 
